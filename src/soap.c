8e9d814997f8db53e1acddd37b2328cd89506164
Petr Pisar <petr.pisar@atlas.cz>
2009-10-17 22:10:00 +0200

    soap() return real SOAP response now

--- src/soap.c
+++ src/soap.c
@@ -2,6 +2,42 @@
 #include "soap.h"
 #include "utils.h"
 #include <stdlib.h>
+#include <string.h>
+
+/* Private structure for write_body() call back */
+struct soap_body {
+    void *data;
+    size_t length;
+};
+
+
+/* CURL call back function called when chunk of HTTP reponse body is available.
+ * @buffer points to new data
+ * @size * @nmemb is length of the chunk in bytes. Zero means empty body.
+ * @userp is private structure.
+ * Must reuturn the length of the chunk, otherwise CURL will signal
+ * CURL_WRITE_ERROR. */
+static size_t write_body(void *buffer, size_t size, size_t nmemb, void *userp) {
+    struct soap_body *body = (struct soap_body *) userp;
+    void *new_data;
+
+    /* FIXME: Check for (size * nmemb + body->lengt) !> SIZE_T_MAX.
+     * Precompute the product then. */
+
+    if (!body) return 0;    /* This should never happen */
+    if (0 == (size * nmemb)) return 0; /* Empty body */
+
+    new_data = realloc(body->data, body->length + size * nmemb);
+    if (!new_data) return 0;
+
+    memcpy(new_data + body->length, buffer, size * nmemb);
+
+    body->data = new_data;
+    body->length += size * nmemb;
+
+    return (size * nmemb);
+}
+
 
 /* Do SOAP request.
  * @file is a (CGI) file of SOAP URL,
@@ -13,11 +49,13 @@
  * @length (does not need to match allocates memory exactly. You must free() the
  * @response. */
 _hidden isds_error soap(struct isds_ctx *context, const char *file,
-        const char *request, void **response, size_t *length ) {
+        const char *request, void **response, size_t *length) {
 
     CURLcode curl_err;
     char *url;
     isds_error err = IE_SUCCESS;
+    struct soap_body body;
+
 
     if (!context) return IE_INVALID_CONTEXT;
     if (!response || !length) return IE_INVAL;
@@ -28,6 +66,15 @@ _hidden isds_error soap(struct isds_ctx *context, const char *file,
     curl_err = curl_easy_setopt(context->curl, CURLOPT_URL, url);
     if (!curl_err) {
         curl_err = curl_easy_setopt(context->curl, CURLOPT_FAILONERROR, 1);
+    }
+    if (!curl_err) {
+        curl_err = curl_easy_setopt(context->curl, CURLOPT_WRITEFUNCTION,
+                write_body);
+    }
+    if (!curl_err) {
+        body.data = NULL;
+        body.length = 0;
+        curl_err = curl_easy_setopt(context->curl, CURLOPT_WRITEDATA, &body);
     }
 
     if (curl_err) {
@@ -45,7 +92,8 @@ _hidden isds_error soap(struct isds_ctx *context, const char *file,
         goto leave;
     }
 
-    *length = 0; 
+    *response = body.data;
+    *length = body.length;
 
 leave:
     free(url);
@@ -55,3 +103,5 @@ leave:
     }
     return err;
 }
+
+
