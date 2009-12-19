1be51527011a8d99e6e795d367f1861fa8a2ad09
Petr Pisar <petr.pisar@atlas.cz>
2009-12-19 19:22:51 +0100

    Add login test

---
+++ test/login.c
@@ -0,0 +1,60 @@
+#include "test.h"
+#include "isds.h"
+
+static int test_login(const isds_error error, struct isds_ctx *context,
+        const char *url, const char *username, const char *password,
+        const char *certificate, const char *key) {
+
+    if (error !=
+            isds_login(context, url, username, password, certificate, key))
+        FAIL_TEST("Wrong return code");
+
+    isds_logout(context);
+    PASS_TEST;
+}
+
+
+int main(int argc, char **argv) {
+    INIT_TEST("login");
+    
+    struct isds_ctx *context = NULL;
+    char url[] = "https://www.czebox.cz/DS/";
+    char username[] = "jrfh7i";
+    char password[] = "Ab123456";
+
+    if (isds_init()) {
+        printf("isds_init() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    context = isds_ctx_create();
+    if (!context) {
+        printf("isds_ctx_create() failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    TEST("invalid context", test_login, IE_INVALID_CONTEXT, NULL,
+            url, username, password, NULL, NULL);
+    TEST("NULL url", test_login, IE_INVAL, context,
+            NULL, username, password, NULL, NULL);
+    TEST("NULL username", test_login, IE_INVAL, context,
+            url, NULL, password, NULL, NULL);
+    TEST("NULL password", test_login, IE_INVAL, context,
+            url, username, NULL, NULL, NULL);
+
+    TEST("invalid URL", test_login, IE_NETWORK, context,
+            "invalid://", username, password, NULL, NULL);
+    TEST("unresolvable host name", test_login, IE_NETWORK, context,
+            "http://example.com/", username, password, NULL, NULL);
+
+    TEST("invalid credentials", test_login, IE_NOT_LOGGED_IN, context,
+            url, "7777777", "nbuusr1", NULL, NULL);
+
+    TEST("valid login", test_login, IE_SUCCESS, context,
+            url, username, password, NULL, NULL);
+
+    isds_ctx_free(&context);
+    isds_cleanup();
+
+    SUM_TEST();
+}
