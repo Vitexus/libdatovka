74503a6dbfd7daad59d2672cf888de569c1000dd
Petr Písař <petr.pisar@atlas.cz>
2010-01-29 11:43:49 +0100

    client: Track changepassword.c

---
+++ client/changepassword.c
@@ -0,0 +1,98 @@
+#define _XOPEN_SOURCE 500
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <time.h>
+#include <string.h>
+#include <isds.h>
+#include "common.h"
+
+void change_password(struct isds_ctx *ctx,
+        const char *old_password, const char *new_password) {
+    printf("Changing password to: `%s'\n", new_password);
+    if (old_password && new_password && !strcmp(new_password, password))
+        printf("(Same as old password)\n");
+    if (!old_password)
+        printf("(Old password omitted)\n");
+    isds_error err = isds_change_password(ctx, old_password, new_password);
+    if (err) {
+        printf("isds_change_password() failed: %s: %s\n",
+                isds_strerror(err), isds_long_message(ctx));
+    } else {
+        printf("isds_change_password() succeeded.\n"
+                "This should not happen");
+    }
+    printf("\n");
+}
+
+int main(int argc, char **argv) {
+    struct isds_ctx *ctx = NULL;
+    isds_error err;
+    
+    setlocale(LC_ALL, "");
+
+    err = isds_init();
+    if (err) {
+        printf("isds_init() failed: %s\n", isds_strerror(err));
+        exit(EXIT_FAILURE);
+    }
+
+    isds_set_logging(ILF_ALL & ~ILF_HTTP, ILL_ALL);
+
+    ctx = isds_ctx_create();
+    if (!ctx) {
+        printf("isds_ctx_create() failed");
+    }
+
+    err = isds_set_timeout(ctx, 10000);
+    if (err) {
+        printf("isds_set_timeout() failed: %s\n", isds_strerror(err));
+    }
+
+    /* err = isds_set_tls(ctx, ITLS_VERIFY_SERVER, 0);
+    if (err) {
+        printf("isds_set_tls(ITLS_VERIFY_SERVER) failed: %s\n",
+                isds_strerror(err));
+    }
+
+    err = isds_set_tls(ctx, ITLS_CA_FILE, "/etc/ssl/certs/ca-certificates.crt");
+    if (err) {
+        printf("isds_set_tls(ITLS_CA_FILE) failed: %s\n",
+                isds_strerror(err));
+    }*/
+
+    err = isds_login(ctx, url, username, password, NULL, NULL);
+    if (err) {
+        printf("isds_login() failed: %s: %s\n", isds_strerror(err),
+                isds_long_message(ctx));
+    } else {
+        printf("Logged in :)\n");
+    }
+
+
+    /* Try some invalid invocation that should fail */
+    change_password(ctx, password, NULL);
+    change_password(ctx, password, "");
+    change_password(ctx, password, password);
+
+
+
+    err = isds_logout(ctx);
+    if (err) {
+        printf("isds_logout() failed: %s\n", isds_strerror(err));
+    }
+
+
+    err = isds_ctx_free(&ctx);
+    if (err) {
+        printf("isds_ctx_free() failed: %s\n", isds_strerror(err));
+    }
+
+
+    err = isds_cleanup();
+    if (err) {
+        printf("isds_cleanup() failed: %s\n", isds_strerror(err));
+    }
+
+    exit (EXIT_SUCCESS);
+}
