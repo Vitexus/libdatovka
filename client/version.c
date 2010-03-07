cc4af5cd6f6e6a4e9b1fedddd47678ae4da6b590
Petr Písař <petr.pisar@atlas.cz>
2010-03-07 19:15:00 +0100

    Add missing client/version.c

---
+++ client/version.c
@@ -0,0 +1,30 @@
+#define _XOPEN_SOURCE 500
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <time.h>
+#include <string.h>
+#include <isds.h>
+#include "common.h"
+
+
+int main(int argc, char **argv) {
+    isds_error err;
+    
+    setlocale(LC_ALL, "");
+
+    err = isds_init();
+    if (err) {
+        printf("isds_init() failed: %s\n", isds_strerror(err));
+        exit(EXIT_FAILURE);
+    }
+
+    printf("libisds version: %s\n", isds_version());
+
+    err = isds_cleanup();
+    if (err) {
+        printf("isds_cleanup() failed: %s\n", isds_strerror(err));
+    }
+
+    exit (EXIT_SUCCESS);
+}
