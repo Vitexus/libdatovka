8b0b15ee47c35b38d6d86376742fd98c97812d8f
Petr Písař <petr.pisar@atlas.cz>
2010-04-19 21:15:11 +0200

    test: guess_raw_type: plain signed incoming message

--- test/guess_raw_type.c
+++ test/guess_raw_type.c
@@ -46,7 +46,12 @@ int main(int argc, char **argv) {
             .type = RAWTYPE_INCOMING_MESSAGE,
             .error = IE_SUCCESS
         },
-        /* FIXME: test plain signed incoming messages */
+        {
+            .name = "plain signed incoming message",
+            .file = "../server/messages/received_signed_message-330141.xml",
+            .type = RAWTYPE_PLAIN_SIGNED_INCOMING_MESSAGE,
+            .error = IE_SUCCESS
+        },
         {
             .name = "CMS signed incoming message",
             .file = "../server/messages/received_message-330141.zfo",
