97f4522442447c49360f7606a0cbdffde5720909
Petr Písař <petr.pisar@atlas.cz>
2010-07-24 21:00:20 +0200

    test: Add generic timestring2timeval

---
+++ test/isds-timestring2timeval.c
@@ -0,0 +1,77 @@
+#include "test.h"
+#include "isds.c"
+
+static int test_timestring2timeval(const xmlChar *time, const isds_error error,
+        const struct timeval *correct_timeval, struct timeval **new_timeval) {
+    isds_error err;
+
+    err = timestring2timeval(time, new_timeval);
+    if (err != error)
+        FAIL_TEST("timestring2timeval() returned unexpected code: "
+                "expected=%s got=%s", isds_strerror(error), isds_strerror(err));
+
+    if (err) {
+        if (new_timeval && *new_timeval)
+            FAIL_TEST("timestring2timeval() failed as exprected "
+                    "but did not freed output timeval structure")
+        else
+            PASS_TEST;
+    }
+
+    if (!correct_timeval) {
+        if (!new_timeval  || !*new_timeval)
+            PASS_TEST
+        else
+            FAIL_TEST("timestring2timeval() did not returned NULL output "
+                    "timeval struct as expected");
+    }
+
+    if (new_timeval && !*new_timeval)
+        FAIL_TEST("timestring2timeval() freed output timeval struct "
+                "unexpectedly");
+
+    if (correct_timeval->tv_sec != (*new_timeval)->tv_sec)
+        FAIL_TEST("Returned struct timeval differs in tv_sec: expected=%d, got=%d",
+                correct_timeval->tv_sec, (*new_timeval)->tv_sec);
+    if (correct_timeval->tv_usec != (*new_timeval)->tv_usec)
+        FAIL_TEST("Returned struct timeval differs in tv_usec: expected=%d, got=%d",
+                correct_timeval->tv_usec, (*new_timeval)->tv_usec);
+
+    PASS_TEST;
+}
+
+
+int main(int argc, char **argv) {
+    INIT_TEST("ISO date-time string to timeval conversion");
+
+    struct timeval *output;
+    char *input = "2001-02-03T04:05:06.123456+01:45";
+    struct timeval time = {.tv_sec = 981166806, .tv_usec = 123456};
+    TEST(input, test_timestring2timeval, BAD_CAST input, IE_SUCCESS, &time,
+            &output);
+
+    /*input = "20010203";
+    TEST(input, test_datestring2tm, BAD_CAST input, IE_SUCCESS, &date, &output);
+
+    input = "2001-34";
+    TEST(input, test_datestring2tm, BAD_CAST input, IE_SUCCESS, &date, &output);
+
+
+    input = "2001-02-03T05:06";
+    TEST(input, test_datestring2tm, BAD_CAST input, IE_NOTSUP, &date, &output);*/
+
+    input = "foo bar";
+    TEST(input, test_timestring2timeval, BAD_CAST input, IE_DATE, &time,
+            &output);
+
+    TEST("Empty input", test_timestring2timeval, BAD_CAST "", IE_DATE, &time,
+            &output);
+
+    TEST("NULL input", test_timestring2timeval, NULL, IE_INVAL, &time,
+            &output);
+
+    TEST("NULL output pointer", test_timestring2timeval, BAD_CAST "", IE_INVAL,
+            NULL, NULL);
+
+    SUM_TEST();
+}
