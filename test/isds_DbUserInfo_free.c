e6c11cd117dea5a76723d8908ffb79794dd1676a
Petr Písař <petr.pisar@atlas.cz>
2010-08-01 11:54:13 +0200

    test: Add isds_DbUserInfo_free

---
+++ test/isds_DbUserInfo_free.c
@@ -0,0 +1,46 @@
+#include "test.h"
+#include "isds.h"
+
+static int test_isds_DbUserInfo_free(
+        struct isds_DbUserInfo **DbUserInfo) {
+    isds_DbUserInfo_free(DbUserInfo);
+    if (!DbUserInfo) PASS_TEST;
+
+    if (*DbUserInfo)
+        FAIL_TEST("isds_DbUserInfo_free() did not null pointer");
+
+    PASS_TEST;
+}
+
+
+int main(int argc, char **argv) {
+
+    INIT_TEST("isds_DbUserInfo_free()");
+    if (isds_init())
+        ABORT_UNIT("isds_init() failed");
+    
+    struct isds_DbUserInfo *DbUserInfo = NULL;
+    TEST("NULL", test_isds_DbUserInfo_free, NULL);
+    TEST("*NULL", test_isds_DbUserInfo_free, &DbUserInfo);
+
+    TEST_CALLOC(DbUserInfo);
+    TEST("Empty structure", test_isds_DbUserInfo_free, &DbUserInfo);
+
+    TEST_CALLOC(DbUserInfo);
+    TEST_FILL_STRING(DbUserInfo->userID);
+    TEST_FILL_INT(DbUserInfo->userType)
+    TEST_FILL_INT(DbUserInfo->userPrivils);
+    TEST_CALLOC(DbUserInfo->personName);       /* Name of the person */
+    TEST_CALLOC(DbUserInfo->address);          /* Post address */
+    TEST_CALLOC(DbUserInfo->biDate);           /* Date of birth in local time */
+    TEST_FILL_STRING(DbUserInfo->ic);
+    TEST_FILL_STRING(DbUserInfo->firmName);
+    TEST_FILL_STRING(DbUserInfo->caStreet);
+    TEST_FILL_STRING(DbUserInfo->caCity);
+    TEST_FILL_STRING(DbUserInfo->caZipCode);
+    TEST_FILL_STRING(DbUserInfo->caState);
+    TEST("Full structure", test_isds_DbUserInfo_free, &DbUserInfo);
+
+    isds_cleanup();
+    SUM_TEST();
+}
