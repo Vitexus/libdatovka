b6f02b21efaf7fe14dda18e8dedc012ad55b6049
Petr Písař <petr.pisar@atlas.cz>
2010-08-01 12:01:20 +0200

    test: Add isds_event_free

---
+++ test/isds_event_free.c
@@ -0,0 +1,37 @@
+#include "test.h"
+#include "isds.h"
+
+static int test_isds_event_free(
+        struct isds_event **event) {
+    isds_event_free(event);
+    if (!event) PASS_TEST;
+
+    if (*event)
+        FAIL_TEST("isds_event_free() did not null pointer");
+
+    PASS_TEST;
+}
+
+
+int main(int argc, char **argv) {
+
+    INIT_TEST("isds_event_free()");
+    if (isds_init())
+        ABORT_UNIT("isds_init() failed");
+    
+    struct isds_event *event = NULL;
+    TEST("NULL", test_isds_event_free, NULL);
+    TEST("*NULL", test_isds_event_free, &event);
+
+    TEST_CALLOC(event);
+    TEST("Empty structure", test_isds_event_free, &event);
+
+    TEST_CALLOC(event);
+    TEST_CALLOC(event->time);           /* When the event occurred */
+    TEST_FILL_INT(event->type);
+    TEST_FILL_STRING(event->description);
+    TEST("Full structure", test_isds_event_free, &event);
+
+    isds_cleanup();
+    SUM_TEST();
+}
