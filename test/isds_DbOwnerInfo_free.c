36ceb08ae678a88e34294d0d423d766937a3186a
Petr Písař <petr.pisar@atlas.cz>
2010-08-08 11:22:52 +0200

    test: fill DbOwnerInfo recursively

--- test/isds_DbOwnerInfo_free.c
+++ test/isds_DbOwnerInfo_free.c
@@ -26,14 +26,35 @@ int main(int argc, char **argv) {
     TEST_CALLOC(DbOwnerInfo);
     TEST("Empty structure", test_isds_DbOwnerInfo_free, &DbOwnerInfo);
 
+
+    /* Full structure */
     TEST_CALLOC(DbOwnerInfo);
     TEST_FILL_STRING(DbOwnerInfo->dbID);
     TEST_FILL_INT(DbOwnerInfo->dbType);
     TEST_FILL_STRING(DbOwnerInfo->ic);
+
     TEST_CALLOC(DbOwnerInfo->personName);       /* Name of person */
+    TEST_FILL_STRING(DbOwnerInfo->personName->pnFirstName);
+    TEST_FILL_STRING(DbOwnerInfo->personName->pnMiddleName);
+    TEST_FILL_STRING(DbOwnerInfo->personName->pnLastName);
+    TEST_FILL_STRING(DbOwnerInfo->personName->pnLastNameAtBirth);
+
     TEST_FILL_STRING(DbOwnerInfo->firmName);
+
     TEST_CALLOC(DbOwnerInfo->birthInfo);        /* Birth of person */
+    TEST_CALLOC(DbOwnerInfo->birthInfo->biDate);      /* Date of Birth */
+    TEST_FILL_STRING(DbOwnerInfo->birthInfo->biCity);
+    TEST_FILL_STRING(DbOwnerInfo->birthInfo->biCounty); 
+    TEST_FILL_STRING(DbOwnerInfo->birthInfo->biState);
+
     TEST_CALLOC(DbOwnerInfo->address);          /* Post address */
+    TEST_FILL_STRING(DbOwnerInfo->address->adCity);
+    TEST_FILL_STRING(DbOwnerInfo->address->adStreet);
+    TEST_FILL_STRING(DbOwnerInfo->address->adNumberInStreet);
+    TEST_FILL_STRING(DbOwnerInfo->address->adNumberInMunicipality);
+    TEST_FILL_STRING(DbOwnerInfo->address->adZipCode);
+    TEST_FILL_STRING(DbOwnerInfo->address->adState);
+
     TEST_FILL_STRING(DbOwnerInfo->nationality);
     TEST_FILL_STRING(DbOwnerInfo->email);
     TEST_FILL_STRING(DbOwnerInfo->telNumber);
