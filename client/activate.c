847977639682011907c2af032c7e586ced1e2620
Petr Písař <petr.pisar@atlas.cz>
2010-12-06 23:31:45 +0100

    client: Add activate

---
+++ client/activate.c
@@ -0,0 +1,92 @@
+#define _XOPEN_SOURCE 500
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#include <isds.h>
+#include "common.h"
+
+
+int main(int argc, char **argv) {
+    struct isds_ctx *ctx = NULL;
+    isds_error err;
+    
+    setlocale(LC_ALL, "");
+
+    err = isds_init();
+    if (err) {
+        printf("isds_init() failed: %s\n", isds_strerror(err));
+        exit(EXIT_FAILURE);
+    }
+
+    isds_set_logging(ILF_ALL & ~ILF_HTTP, ILL_ALL);
+
+    ctx = isds_ctx_create();
+    if (!ctx) {
+        printf("isds_ctx_create() failed");
+    }
+
+    err = isds_set_timeout(ctx, 10000);
+    if (err) {
+        printf("isds_set_timeout() failed: %s\n", isds_strerror(err));
+    }
+
+    /* err = isds_set_tls(ctx, ITLS_VERIFY_SERVER, 0);
+    if (err) {
+        printf("isds_set_tls(ITLS_VERIFY_SERVER) failed: %s\n",
+                isds_strerror(err));
+    }
+
+    err = isds_set_tls(ctx, ITLS_CA_FILE, "/etc/ssl/certs/ca-certificates.crt");
+    if (err) {
+        printf("isds_set_tls(ITLS_CA_FILE) failed: %s\n",
+                isds_strerror(err));
+    }*/
+
+    err = isds_login(ctx, url, username, password, NULL);
+    if (err) {
+        printf("isds_login() failed: %s: %s\n", isds_strerror(err),
+                isds_long_message(ctx));
+    } else {
+        printf("Logged in :)\n");
+    }
+
+
+    {
+        char *user_id = NULL, *password = NULL;
+        /* Try a call, not our box as we do not know what does it */
+        err = isds_activate(ctx, "kuqcfr6", "token", &user_id, &password);
+        if (err) {
+            printf("isds_activate() failed: %s: %s\n",
+                    isds_strerror(err), isds_long_message(ctx));
+        } else {
+            printf("isds_activate() succeeded:\n");
+            printf("user_id = %s\n", user_id);
+            printf("password = %s\n", password);
+        }
+        printf("\n");
+
+        free(user_id);
+        free(password);
+    }
+
+
+    err = isds_logout(ctx);
+    if (err) {
+        printf("isds_logout() failed: %s\n", isds_strerror(err));
+    }
+
+
+    err = isds_ctx_free(&ctx);
+    if (err) {
+        printf("isds_ctx_free() failed: %s\n", isds_strerror(err));
+    }
+
+
+    err = isds_cleanup();
+    if (err) {
+        printf("isds_cleanup() failed: %s\n", isds_strerror(err));
+    }
+
+    exit (EXIT_SUCCESS);
+}
