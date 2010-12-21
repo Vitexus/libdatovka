1c3e70fdffbea2b8efe0a666090411007de1b6f0
Petr Písař <petr.pisar@atlas.cz>
2010-12-21 19:36:51 +0100

    Check fake .gnupg directory is writable

--- test/prepare_environment.c
+++ test/prepare_environment.c
@@ -4,15 +4,18 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <unistd.h>
 
 #define GNUPG_DIR ".gnupg"
 
 static int test_setup_gnupg(void) {
     char *home, *path = NULL;
+    char tmpfile[] = "XXXXXX";
+    int tmpfd;
 
     home = getenv("HOME");
     if (!home) 
-        FAIL_TEST("Could not get home directory");
+        FAIL_TEST("Could not get HOME variable");
 
     if (!*home)
         FAIL_TEST("HOME environment variable is empty");
@@ -26,6 +29,23 @@ static int test_setup_gnupg(void) {
         free(path);
         return 1;
     }
+
+    if (chdir(path)) {
+        FAILURE_REASON("Could not change CWD to `%s': %s", path,
+                strerror(errno));
+        free(path);
+        return 1;
+    }
+
+    if (-1 == (tmpfd = mkstemp(tmpfile))) {
+        FAILURE_REASON("Directory `%s' is not writable: %s", path,
+                strerror(errno));
+        free(path);
+        return 1;
+    }
+
+    close(tmpfd);
+    unlink(tmpfile);
 
     free(path);
     PASS_TEST;
