de74fd52cab852f44994a859f5805cba74bf053b
Petr Písař <petr.pisar@atlas.cz>
2011-02-23 22:42:08 +0100

    client: Pass credentials externally
    You need to pass credentials for example clients by environment or a file.
    See README for more details.

--- client/common.c
+++ client/common.c
@@ -1,4 +1,4 @@
-#define _XOPEN_SOURCE 500
+#define _XOPEN_SOURCE 700
 #include <stdlib.h>
 #include <stdio.h>
 /*#include <locale.h>*/
@@ -15,12 +15,68 @@
 
 char username_value[] = "5s59sd";
 char password_value[] = "Af123456";
+char credentials_file[] = "../test_credentials";
+
+static int read_config(char **line, int order) {
+    FILE *file;
+    size_t length = 0;
+    char *eol;
+
+    if (!line) return -1;
+    free(*line);
+    *line = NULL;
+
+    file = fopen(credentials_file, "r");
+    if (!file) {
+        fprintf(stderr, "Could open %s\n", credentials_file);
+        return -1;
+    }
+
+    for (int i = 0; i < order; i++) {
+        if (-1 == getline(line, &length, file)) {
+            fprintf(stderr, "Could not read line #%d from %s: ",
+                    i + 1, credentials_file);
+            if (ferror(file))
+                fprintf(stderr, "error occured\n");
+            else if (feof(file)) 
+                fprintf(stderr, "end of file reached\n");
+            else
+                fprintf(stderr, "I don't know why\n");
+            fclose(file);
+            free(*line);
+            *line = NULL;
+            return -1;
+        }
+    }
+
+    fclose(file);
+
+    eol = strpbrk(*line, "\r\n");
+    if (eol) *eol = '\0';
+
+    return 0;
+}
 
 const char *username(void) {
-    return username_value;
+    static char *username;
+
+    if (!username) {
+        username = getenv("ISDS_USERNAME");
+        if (!username)
+            read_config(&username, 1);
+    }
+    return username;
 }
+
 const char *password(void) {
-    return password_value;
+    static char *password;
+
+    if (!password) {
+        password = getenv("ISDS_PASSWORD");
+        if (!password)
+            read_config(&password, 2);
+    }
+    return password;
 }
 
 void print_DbState(const long int state) {
