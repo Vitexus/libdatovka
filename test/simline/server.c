4c21a8c6f937e80a1e140224bd88a8ad2ec0547e
Petr Písař <petr.pisar@atlas.cz>
2011-08-10 09:58:02 +0200

    test: server: Send valid ISDS dummy response

--- test/simline/server.c
+++ test/simline/server.c
@@ -113,7 +113,7 @@ static void server(int server_socket,
     int client_socket;
     struct http_request *request = NULL;
     http_error error;
-    const char *pong = "<pong/>";
+    const char *pong = "<?xml version='1.0' encoding='utf-8'?><SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><SOAP-ENV:Body><q:DummyOperationResponse xmlns:q=\"http://isds.czechpoint.cz/v20\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><q:dmStatus><q:dmStatusCode>0000</q:dmStatusCode><q:dmStatusMessage>Provedeno úspěšně.</q:dmStatusMessage></q:dmStatus></q:DummyOperationResponse></SOAP-ENV:Body></SOAP-ENV:Envelope>";
 
     while (0 <= (client_socket = accept(server_socket, NULL, NULL))) {
         fprintf(stderr, "Connection accepted\n");
